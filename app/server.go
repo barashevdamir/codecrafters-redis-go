f65e36b3f31e30f92a8d9e6104eee4af9683e8dd
Дамир Барашев <barashev-01@mail.ru>
2024-06-26 07:00:19 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	
+
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -15,9 +15,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Received connection")
+	conn.Write([]byte("+PONG\r\n"))
 }
